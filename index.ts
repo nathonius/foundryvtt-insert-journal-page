26c1ccfb992e53d50cec05c19fa459f3e9da3210
Nathan Smith <nsmith1@onetrust.com>
2022-09-28 14:03:39 -0400

    feat: initial release

---
+++ index.ts
@@ -0,0 +1,49 @@
+import { libWrapper } from "./shim.js";
+
+Hooks.once("init", () => {
+  libWrapper.register(
+    "insert-journal-page",
+    "JournalSheet.prototype.createPage",
+    afterCreatePage,
+    "WRAPPER"
+  );
+});
+
+async function afterCreatePage(
+  this: JournalSheet & {
+    pageIndex: number;
+    _pages: any;
+  },
+  wrapped: () => Promise<JournalEntry>
+) {
+  const currentPage = this._pages.at(this.pageIndex);
+  const nextPage = this._pages.at(this.pageIndex + 1);
+
+  // If less than two pages or current page is last, do nothing special.
+  if (
+    this._pages.size < 2 ||
+    this.pageIndex === this._pages.size - 1 ||
+    !currentPage ||
+    !nextPage
+  ) {
+    return wrapped();
+  }
+
+  // New sort value should be halfways between current and next page
+  const newSortValue =
+    Math.round((nextPage.sort - currentPage.sort) / 2) + currentPage.sort;
+
+  // Create new page
+  let newPage = await wrapped();
+
+  // Update sort
+  if (newPage) {
+    const updated = await newPage.update({ sort: newSortValue });
+    if (updated) {
+      newPage = updated;
+    }
+  }
+
+  // Return output
+  return newPage;
+}
